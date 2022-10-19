33473d112e0b00bcf2f1a62d07a37115103551d0
dmcbdh31 <111210876+dmcbdh31@users.noreply.github.com>
2022-10-19 16:38:09 -0400

    Add files via upload

---
+++ DayTrip.py
@@ -0,0 +1,44 @@
+
+
+destinations = ['Cincinnati', 'Kings Island', 'Holiday World']
+
+for destination in destinations:
+    print(destination)
+
+restaurants = ['Skyline Chili', 'La Rosas', 'Chick-Fil-A']
+
+for restaurant in restaurants:
+    print(restaurant)
+
+mode_of_transportation = ['Rental Car', 'My Car', 'Airplane']
+
+for transportation in mode_of_transportation:
+    print(transportation)
+
+entertainment = ['baseball game', 'Roller Coasters', 'Waterpark']
+
+for entertainment in entertainment:
+    print(entertainment)
+
+
+my_first_day_trip_dictionary = {
+    "Cincinnati": "A family outing to a baseball game.",
+    "Rental Car": "Rent a car for the day.",
+    "Skyline Chili": "Have some Cincinnati own Skyline Chili for lunch.",
+    "baseball game": "Enjoy a late afternoon game at Great American Ballpark."
+}
+
+my_second_day_trip_dictionary = {
+    "Kings Island": "Fun for the whole family.",
+    "My Car": "Close enough to drive your own car.",
+    "La Rosas": "Have a La Rosas pizza.",
+    "Roller Coasters": "Enjoy the many roller coasters or other rides."
+}
+
+my_third_day_trip_dictionary = {
+    "Holiday World": "Visit Santa in July.",
+    "Rental Car": "Take a rental car for the drive.",
+    "Chick-Fil-A": "No need for Chick-Fil-A, plenty of choices inside the park.",
+    "Waterpark": "Spend the day at the waterpark."
+}
+
